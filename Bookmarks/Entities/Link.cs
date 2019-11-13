0fd6a8aa9b1b36e046b3ea80be1a537689acb03e
mehedi <mehedirumc12cuet14@gmail.com>
2019-11-14 00:23:12 +0600

    Adding Link table

---
+++ Bookmarks/Entities/Link.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Bookmarks.Entities
+{
+    public class Link
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        [MaxLength(255)]
+        public string Title { get; set; }
+
+        [MaxLength(2048)]
+        public string Description { get; set; }
+
+        [Required]
+        [MaxLength(1024)]
+        public string Url { get; set; }
+
+    }
+}
---
+++ Bookmarks/Migrations/201911131819315_AddLinkTable.cs
@@ -0,0 +1,29 @@
+﻿namespace Bookmarks.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddLinkTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Links",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 255),
+                        Title = c.String(maxLength: 255),
+                        Description = c.String(maxLength: 2048),
+                        Url = c.String(nullable: false, maxLength: 1024),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Links");
+        }
+    }
+}
